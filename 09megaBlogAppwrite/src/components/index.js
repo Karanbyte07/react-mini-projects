1d0181c48838bcad6457ee588e2eaecc5439d7fe
karanjeet kumar <karanbyte07@gmail.com>
2026-01-12 17:34:43 +0530

    enhance header component with navigation and authentication, add reusable button and input components, and implement footer layout

--- 09megaBlogAppwrite/src/components/index.js
+++ 09megaBlogAppwrite/src/components/index.js
@@ -1,7 +1,13 @@
-import Header from './Header/Header.jsx';
-import Footer from './Footer/Footer.jsx';
+import Header from './header/header.jsx'
+import Footer from './footer/footer.jsx'
+import Logo from './logo.jsx'
+import Container from './container/container.jsx'
+import LogoutBtn from './header/logoutBtn.jsx'
 
 export {
     Header,
-    Footer
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn
 }
