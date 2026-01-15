27f7ce7515b066b2debc00bb56364b64ad211adf
karanjeet kumar <karanbyte07@gmail.com>
2026-01-15 16:13:39 +0530

    implement post form with rich text editor and file upload functionality

--- 09megaBlogAppwrite/src/components/authLayout.jsx
+++ 09megaBlogAppwrite/src/components/authLayout.jsx
@@ -2,20 +2,21 @@ import { use, useEffect, useState } from "react"
 import {useSelector} from "react-redux"
 import {useNavigate} from "react-router-dom"
 
-export default function Protected({children, authentication = true }) {
+export default function Protected({children, authentication = true }) { //children is a page content to sisplay, authentication is a boolean to check if the page requires authentication or not
     const navigate = useNavigate()
-    const [loader, setLoader] = useState(true)
-    const authStatus = useSelector((state) => state.auth.status)
+    const [loader, setLoader] = useState(true) //show loader while checking authentication status
+    const authStatus = useSelector((state) => state.auth.status)  //check user is logged in or not from redux store
 
     useEffect(() => {
-        if(authentication && authStatus !== authentication){
+        if(authentication && authStatus !== authentication){ //if the page requires login but user is not logged in
             navigate('/login')
-        }else if(!authentication && authStatus !== authentication){
+        }else if(!authentication && authStatus !== authentication){ //if the page does not require login but user is logged in
             navigate('/')
         }
-        setLoader(false)
+        setLoader(false) //stop loader after checking authentication status
     }), [authStatus, navigate, authentication]
 
   return loader ? <div>Loading...</div> : <>{children}</>
 }
 
+// This file creates a Protected Route component that controls access to different pages based on whether a user is logged in or not.
--- 09megaBlogAppwrite/src/components/index.js
+++ 09megaBlogAppwrite/src/components/index.js
@@ -3,11 +3,20 @@ import Footer from './footer/footer.jsx'
 import Logo from './logo.jsx'
 import Container from './container/container.jsx'
 import LogoutBtn from './header/logoutBtn.jsx'
+import Button from './button.jsx'
+import Select from './select.jsx'
+import Input from './input.jsx'
+import RTE from './RTE.jsx'
 
 export  {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Select,
+    Input,
+    RTE
+    
 }
